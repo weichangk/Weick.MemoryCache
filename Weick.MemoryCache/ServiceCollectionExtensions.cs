8961f522e111c01077e09a9d48dca8d3d86d5d34
weichangk <18276743761@163.com>
2022-04-08 20:37:33 +0800

    add

---
+++ Weick.MemoryCache/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Weick.MemoryCache
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 添加Cache缓存
+        /// </summary>
+        /// <param name="services"></param>
+        public static IServiceCollection AddCache(this IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddSingleton<ICacheHandler, MemoryCacheHandler>();
+            return services;
+        }
+    }
+}
